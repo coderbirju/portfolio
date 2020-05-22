4b2222077d0fc717f5b611e4c1551e53ba7a569b
Arjun Raja <arjunraja@cybrilla.com>
2020-05-22 17:23:24 +0530

    added antd

---
+++ src/templates/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../global-css.css';
+
+function Homepage() {
+  return (
+    <div className="App">
+      <body className="App App-body">
+          <h3> Hi I'm Arjun Raja Y </h3>
+          <p> Fullstack Web Developer and Software Engineer. Welcome to my Website </p>
+      </body>
+    </div>
+  );
+}
+
+export default Homepage;
