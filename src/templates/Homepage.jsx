174411da1ad6a56b41ffa4734a1ab3252de11bc3
Arjun Raja <arjunraja@cybrilla.com>
2020-06-07 20:56:35 +0530

    added color to navbar

--- src/templates/Homepage.jsx
+++ src/templates/Homepage.jsx
@@ -7,15 +7,15 @@ const { Content } = Layout;
 class Homepage extends React.Component {
   render (){
     return (
-      <div className="App">
       <Layout>
-        <Content className="App App-body">          
-            <img src={profilePic} alt="ismet" className="App-logo profile-img"></img>
-              <h3> Hi I'm Arjun Raja Y </h3>
-              <p> Fullstack Web Developer and Software Engineer. Welcome to my Website </p>
-        </Content>
+        <div className="App App-body">
+          <Content> 
+              <img src={profilePic} alt="ismet" className="App-logo profile-img"></img>
+                <h3> Hi I'm Arjun Raja Y </h3>
+                <p> Fullstack Web Developer and Software Engineer. Welcome to my Website </p>
+          </Content>
+        </div>
       </Layout>
-      </div>
     );
   }
 }
