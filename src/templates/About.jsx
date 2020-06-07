b144744f76c3d0fad7b060c014221e9fc59b8d77
Arjun Raja <arjunraja@cybrilla.com>
2020-06-07 15:33:27 +0530

    Changes

--- src/templates/About.jsx
+++ src/templates/About.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import '../global-css.css';
-import Header from './header';
+import HeadNav from './header';
+import { Layout } from 'antd';
+const { Header,Content } = Layout;
 
 class About extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <body className="App App-body">
-            <h3> About ME </h3>
-            <p> AVOVOJIVAIV </p>
-        </body>
+        <Layout>
+          <Header>
+            <HeadNav />
+          </Header>
+          <Content>
+            <div className="App">
+              <body className="App App-body">
+                  <h3> About ME </h3>
+                  <p> AVOVOJIVAIV </p>
+              </body>
+            </div>
+          </Content>
+        </Layout>
       </div>
+      
     );
   }
 }
--- src/templates/Homepage.jsx
+++ src/templates/Homepage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import '../global-css.css';
 import profilePic from '../linkedin.png';
-import Header from './header';
+import HeadNav from './header';
+import { Layout } from 'antd';
+const { Header,Content } = Layout;
 
 class Homepage extends React.Component {
   render (){
     return (
       <div className="App">
-      <Header/> 
-        <body className="App App-body">
-          <img src={profilePic} alt="ismet" className="App-logo profile-img"></img>
-            <h3> Hi I'm Arjun Raja Y </h3>
-            <p> Fullstack Web Developer and Software Engineer. Welcome to my Website </p>
-        </body>
+      <Layout>
+        <Header> <HeadNav/> </Header>
+        <Content>
+          <body className="App App-body">
+            <img src={profilePic} alt="ismet" className="App-logo profile-img"></img>
+              <h3> Hi I'm Arjun Raja Y </h3>
+              <p> Fullstack Web Developer and Software Engineer. Welcome to my Website </p>
+          </body>
+        </Content>
+      </Layout>
+       
+        
       </div>
     );
   }
